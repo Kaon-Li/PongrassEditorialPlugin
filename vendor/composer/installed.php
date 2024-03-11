acbdeaa716b1d95d07d71cb9b62a00fec73f26e3
Kaon Li <kaon@pongrass.com.au>
2024-03-11 11:41:08 +1100

    Added composer package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'www.pongrass.net/pep',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0bcda3975312030b9540e2b10ba84723f5e237c3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'www.pongrass.net/pep' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0bcda3975312030b9540e2b10ba84723f5e237c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
